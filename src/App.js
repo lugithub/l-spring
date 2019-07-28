1a9fc92ab55f05a180cf0bded1f71511ba2884f4
lubin <lu.bin.lu.bin@gmail.com>
2019-07-28 10:12:55 -0400

    still no idea how to use it in effect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { useSpring, animated } from 'react-spring';
+import styled from 'styled-components/macro';
+
 function App() {
+  const [props, setProps] = useState();
+  const [down, setDown] = useState(false);
+  const move = useSpring({
+    top: 16,
+    from: { top: 0 },
+    config: { duration: 10000 },
+  });
+  const moveUp = useSpring({
+    top: 0,
+    from: { top: 16 },
+    config: { duration: 10000 },
+  });
+
+  useEffect(() => {
+    down ? setProps(move) : setProps(moveUp);
+  }, [down]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyLabel style={props} onClick={() => setDown(!down)}>
+        I will move
+      </MyLabel>
+      <span> base</span>
+    </>
   );
 }
+
+const MyLabel = styled(animated.span)`
+  position: relative;
+  outline: 1px solid red;
+`;
 
 export default App;
